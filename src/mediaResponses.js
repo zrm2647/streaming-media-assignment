63e062886108dc2fbca4816ef1195d860c209d56
Zachary Michaels <zrm2647@g.rit.edu>
2017-09-26 18:25:36 -0400

    Fixing a CircleCI-caught Error

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,6 +1,57 @@
 const fs = require('fs'); // pull in the file system module
 const path = require('path');
 
+function loadFile(request, response, fileName, contentType){
+    const file = path.resolve(__dirname, fileName);
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
+
+    let range = request.headers.range;
+
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    const positions = range.replace(/bytes=/, '').split('-');
+
+    let start = parseInt(positions[0], 10);
+
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+    if (start > end) {
+      start = end - 1;
+    }
+
+    const chunksize = (end - start) + 1;
+
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': contentType,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
+}
+
 const getParty = (request, response) => {
  /* const file = path.resolve(__dirname, '../client/party.mp4');
 
@@ -60,57 +111,6 @@ const getBling = (request, response) => {
 const getBird = (request, response) => {
     loadFile(request, response, '../client/bird.mp4','video/mp4');
 };
-
-function loadFile(request, response, fileName, contentType){
-    const file = path.resolve(__dirname, fileName);
-
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        response.writeHead(404);
-      }
-      return response.end(err);
-    }
-
-    let range = request.headers.range;
-
-    if (!range) {
-      range = 'bytes=0-';
-    }
-
-    const positions = range.replace(/bytes=/, '').split('-');
-
-    let start = parseInt(positions[0], 10);
-
-    const total = stats.size;
-    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-
-    if (start > end) {
-      start = end - 1;
-    }
-
-    const chunksize = (end - start) + 1;
-
-    response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/${total}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': contentType,
-    });
-
-    const stream = fs.createReadStream(file, { start, end });
-
-    stream.on('open', () => {
-      stream.pipe(response);
-    });
-
-    stream.on('error', (streamErr) => {
-      response.end(streamErr);
-    });
-
-    return stream;
-  });
-}
 
 module.exports.getParty = getParty;
 module.exports.getBling = getBling;
